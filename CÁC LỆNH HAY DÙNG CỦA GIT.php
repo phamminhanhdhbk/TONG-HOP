6397f79a197f6ea686313a08f3e5f1775a171d41
Minh Anh <minhanh.itqn@gmail.com>
2016-09-29 21:43:34 +0700

    1

---
+++ CÁC LỆNH HAY DÙNG CỦA GIT.php
@@ -0,0 +1,16 @@
+git add . // đẩy lên tất cả
+git add * // đẩy lên tất cả
+git commit -m "" // tiêu đề muốn pull lênh
+git tag Name_tag muốn đẩy lên
+git push origin master // đẩy lên nhánh master
+git push origin Name_tag // đẩy lên tag muốn đưa lên
+git init // tạo repostory trên git
+git checkout -b feature_x  // tạo nhánh mới trên git
+git checkout master // trở lại nhánh 
+git branch -d name_branch // xóa nhánh
+git branch // kiểm tra xem mình đang ở nhánh nào
+git branch -v // xem commit mới nhất trên nhánh
+git merge tennhanh  // mới chuyển về nhánh master rồi mới merge
+
+
+đây là nhánh của minhanh
